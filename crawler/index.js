06300de3308de0ab5429dacbe20036790c084682
Dillon Powers <dillon.c.powers@gmail.com>
2017-02-16 14:08:59 -0500

    put cached js files into own directory

--- crawler/index.js
+++ crawler/index.js
@@ -2,13 +2,14 @@ const path = require('path');
 const getSenatorsPages = require('./getSenatorsPages');
 const getFaxNumbers = require('./getFaxNumbers');
 const saveSenators = require('./saveSenators');
+const fs = require('fs');
 
 const rootSenatorPage = 'https://www.senate.gov/senators/contact/';
 
 
 getSenatorsPages(rootSenatorPage)
 	.then(rawSenators => {
-		console.log('Found all links to senator pages...')
+		console.log('Found all links to senator pages...');
 		return Promise.all(rawSenators.map(getFaxNumbers));
 	})
 	.then(allSenators => {
@@ -17,8 +18,17 @@ getSenatorsPages(rootSenatorPage)
 	.then(senatorsWithFaxes => {
 		// console.log(`CHECK OUT THESE SENS:\n `, senatorsWithFaxes);
 		console.log(`\nFound ${senatorsWithFaxes.length} senators with fax numbers.`);
-		const dateString = Date.now().toString();
-		const filePath = path.join(__dirname, `senators_${dateString}.js`);
+		let filePath;
+		if (senatorsWithFaxes.length >= 60) { // only overwrite if no 403s
+			filePath = path.join(__dirname, 'scraped.js');
+		} else {															// cache with timestamp
+			const dateString = Date.now().toString();
+			const dir = path.join(__dirname, 'cached');
+			if (!fs.existsSync(dir)) {
+				fs.mkdirSync(dir);
+			}
+			filePath = path.join(dir, `senators_${dateString}.js`);
+		}
 		return saveSenators(filePath, senatorsWithFaxes);
 	})
 	.then(() => {
